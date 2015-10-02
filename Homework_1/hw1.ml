d491841c6f7d8655cf345968766cca412478d6ed
Tommy Lutton <tlutton@ucla.edu>
2015-10-02 11:47:34 -0700

    can now find all direct and indirectly terminal Ns

--- Homework_1/hw1.ml
+++ Homework_1/hw1.ml
@@ -12,16 +12,20 @@ let rec subset a b = match a with
 let equal_sets a b =
 	if (subset a b) = (subset b a) then true else false ;;
 
-(** returns a list representing the union of 'a' and 'b'. avoids adding duplicates from a to b. **)
+(** returns a list representing the union of 'a' and 'b'. avoids adding 
+	duplicates from a to b. **)
 let rec set_union a b = match a with
 	| [] -> b
-	| h::t -> if is_a_in_b h b then (set_union t b) else set_union t (b @ [h]) ;;
+	| h::t -> if is_a_in_b h b then (set_union t b) 
+			else set_union t (b @ [h]) ;;
 
 (** returns a list representing the intersection of 'a' and 'b' **)
 let set_intersection a b = 
 	let rec aux first inter = match first with
 		| [] -> inter
-		| h::t -> if (is_a_in_b h b) then (aux t (inter @ [h])) else (aux t inter)
+		| h::t -> if (is_a_in_b h b) 
+					then (aux t (inter @ [h])) 
+				else (aux t inter)
 	in aux a [] ;;
 
 (** returns a list representing a-b, the set of all members of a that are not 
@@ -34,9 +38,11 @@ let set_diff a b =
 
 (** returns the computed fixed point for 'f' w.r.t. 'x'
 
-	@Param eq : the equality predicate for f's domain. i.e. (=), the builtin equality predicate
+	@Param eq : the equality predicate for f's domain. i.e. (=), the builtin 
+	equality predicate
 
-	If there is no computed fixed point, the function's behavior is undefined per specification. **)
+	If there is no computed fixed point, the function's behavior is undefined 
+	per specification. **)
 let computed_fixed_point eq f x =  
 	let rec aux y z = 
 		if (eq y z) then y else aux z (f z)
@@ -47,7 +53,8 @@ let computed_fixed_point eq f x =
 	@Param eq : the equality predicate for f's domain **)
 let rec computed_periodic_point eq f p x = 
 	let rec aux q y = 
-		if q = p then (if (eq y x) then x else computed_periodic_point eq f p (f x) )
+		if q = p then (if (eq y x) then x 
+						else computed_periodic_point eq f p (f x) )
 	else aux (q+1) (f y)
 in aux 0 x ;;
 
@@ -56,17 +63,22 @@ in aux 0 x ;;
 
 (** Algorithm:
 
-	1.	Find all N's that evaluate directly to T's. Add those N's to a 'safe' list
+	1.	Find all N's that evaluate directly to T's. Add those N's to a 'safe' 
+		list
 	2.	Find all N's that indirectly evaluate to T's. 
 			A. Look for N's that evaluate to N's already in 'safe' list
 			B. Will need to loop several times removing N's that were already
 				placed into 'safe' list s
-	3.  Remove all rules that either start with or point to nonterminals not in the
-		'safe' list. 
-
+	3.  Remove all rules that either start with or point to nonterminals not in 
+		the 'safe' list. 
 **)
 
-(** This function takes in a symbol and returns true if the symbol is a terminal. false otherwise *)
+type ('nonterminal, 'terminal) symbol =
+  | N of 'nonterminal
+  | T of 'terminal  ;;
+
+(** This function takes in a symbol and returns true if the symbol is a 
+	terminal. false otherwise *)
 let is_terminal x = match x with
 	| T _ -> true
 	| _ -> false ;;
@@ -81,12 +93,14 @@ let rec is_each_symbol_terminal = function
 	| [] -> true
 	| h::t -> if (is_terminal h) then (is_each_symbol_terminal t) else false ;;
 
-(** This function takes in a list of (nonterminal * symbol list) list and the empty list to o. 
+(** This function takes in a list of (nonterminal * symbol list) list and the 
+empty list to o. 
 
-	Returns a list of nonterminals (AKA Ns) that have right sides that only lead to terminals
+	Returns a list of nonterminals (AKA Ns) that have right sides that only 
+	lead to terminals
 
-	Point of the function is to start building the list that will be referenced to determine whether
-	or not an N is contributing to a blind alley. 
+	Point of the function is to start building the list that will be referenced 
+	to determine whether or not an N is contributing to a blind alley. 
 
 	TODO: Should I make a list with list with no duplicates?? *)
 let rec get_Ns_with_all_terminals o = function
@@ -96,10 +110,11 @@ let rec get_Ns_with_all_terminals o = function
 			else (get_Ns_with_all_terminals o t) ;;
 
 
-(** This function takes in a list of nonterminals that currently lead either directly or indirectly
-	to terminals and a list symbols to evaluate. 
+(** This function takes in a list of nonterminals that currently lead either 
+	directly or indirectly to terminals and a list symbols to evaluate. 
 
-	Returns true if all nonterminals in symbols in 2nd input list are found in l.
+	Returns true if all nonterminals in symbols in 2nd input list are found in 
+	l.
 
 *)
 let rec is_each_symbol_ind_terminal l = function
@@ -113,7 +128,8 @@ let rec is_each_symbol_ind_terminal l = function
 
 	Returns a list of nonterminals that can indirectly reach a terminal. 
 
-Goal is that every nonterminal that is NOT in the return list constitutes a blind alley 
+Goal is that every nonterminal that is NOT in the return list constitutes a 
+blind alley 
 
 Thought: This function should be called until N's are no longer added *)
 let rec get_Ns_with_ind_terminals safe = function 
@@ -122,10 +138,40 @@ let rec get_Ns_with_ind_terminals safe = function
 				then ( get_Ns_with_ind_terminals (safe @ [(fst h)]) t )
 			else (get_Ns_with_ind_terminals safe t) ;;
 
+(** ^^^ Should do something about adding duplicates to the list ^^^  **)
 
-(** returns a copy of the grammar g with all blind-alley rules removed while preserving
-	the original order of rules found in g.  **)
-(* let filter_blind_alleys g = *)
+
+
+(** Function that loops get_Ns_with_ind_terminals and stops when safe is no 
+	longer being modified 
+
+	@Parameter: safe = safe list, match/function list = list of symbol lists, *)
+
+let rec loop s l = match l with
+	| [] -> s
+	| h::t -> ( loop (get_Ns_with_ind_terminals s [h]) t );;
+
+
+
+(** Function calls itself @Param: safe and (loop safe llist) are not equal
+	This means that there were Ns found that can reach Ts either directly
+	or indirectly. We should keep looping until no more Ns can reach Ts  *)
+let rec call_get_Ns safe l = match (loop safe l) with
+	h::t -> if (equal_sets (h::t) safe) 
+				then safe 
+			else (call_get_Ns (h::t) l) ;;
+
+
+(** returns a copy of the grammar g with all blind-alley rules removed while 
+	preserving the original order of rules found in g.  
+
+	Create a list of nonterminals that are NOT blind alley Ns. Then look 
+	through snd g and find all instances of Ns not in the list. 
+
+**)
+let filter_blind_alleys g = 
+	
+
 	
 
 
